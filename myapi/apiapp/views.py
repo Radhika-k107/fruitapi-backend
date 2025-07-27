6bc8475a68b947c81b8353e7708040055bd6a2a5
Radhika-k107 <155880614+Radhika-k107@users.noreply.github.com>
2025-07-27 17:05:36 +0530

    Update views.py

--- myapi/apiapp/views.py
+++ myapi/apiapp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from .models import Fruit
 from .serializers import FruitSerializer
 
 class FruitViewSet(viewsets.ModelViewSet):
     queryset = Fruit.objects.all()
     serializer_class = FruitSerializer
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
